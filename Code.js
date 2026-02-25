d0d1f8ad2928690fe995f05495a09239c22672a6
Evan Melet <evanmelet@gmail.com>
2026-02-25 11:26:06 +0100

    Ajouter la structure HTML pour les vidéos et le script de lecture

---
+++ Code.js
@@ -0,0 +1,11 @@
+//constantes
+const playButton = document.querySelector('.play');
+const videoTop = document.querySelector('.video-top video');
+const videoBottom = document.querySelector('.video-bottom video');
+
+//detecter clic sur le bouton play 
+playButton.addEventListener('click', () => {
+    //lancer les deux vidéos
+    videoTop.play();
+    videoBottom.play();
+});kn
